a6f946f29d66cc147f8c439f8a1c12b8a1316d9d
Manu Sharma <manuiiitv@gmail.com>
2017-04-08 23:54:49 +0530

    first commit

---
+++ codes/quadratic.m
@@ -0,0 +1,67 @@
+function quadratic(a,b,c,d,f)
+ 
+min_re = -4;
+max_re = 4;
+min_im = -2;
+max_im = 2;
+n_re = 100;
+n_im = 100;
+tol = 0.01;
+coeff = [a b c d f] ; 
+polyroots = roots(coeff);
+r1=polyroots(1);
+r2=polyroots(2);
+r3=polyroots(3);
+r4=polyroots(4);
+max_steps=30;
+
+delta_re = (max_re-min_re)/n_re; 
+delta_im = (max_im-min_im)/n_im;
+x = min_re:delta_re:max_re; 
+y = min_im:delta_im:max_im;
+[X,Y]=meshgrid(x,y); 
+
+Z = X + i*Y;
+
+for j = 1:n_im + 1
+for k = 1:n_re + 1
+
+z = Z(j,k);
+
+if z == 0
+z = tol;
+end
+
+flag = 0;
+p=0;
+while (flag <= max_steps) && (p==0)
+z = z - (a*z.^4 + b*z.^3 + c*z.^2 +d*z+f)./(4*a*z.^3 + 3*b*z.^2 + 2*c*z + d);
+if norm(a*z.^4 + b*z.^3 + c*z.^2 + d*z +f) <= tol
+p=1;
+end
+end
+if norm(z-r1)<=tol
+Z(j,k)=0;
+
+elseif norm(z-r2)<=tol
+Z(j,k)=110;
+
+elseif norm(z-r3)<=tol
+Z(j,k)=180;
+
+elseif norm(z-r4)<=tol
+Z(j,k)=248;
+
+
+end
+end
+end
+axis([-4 4 -2 2]);
+brighten(0.5);
+
+pcolor(X,Y,Z)
+
+shading flat;
+endfunction
+
+
